26d44927fab162dccfcfca6c8318906ffc7654d0
Maud de La Gournerie mdelagournerie@live.fr <mdelagournerie@live.fr>
2020-05-12 15:44:52 +0200

    model + controller

---
+++ db/migrate/20200512132912_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :address
+      t.string :phone_number
+      t.string :banner
+
+      t.timestamps
+    end
+  end
+end
