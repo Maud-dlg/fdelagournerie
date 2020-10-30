61825a7fafbe3185f5715f1727464f85e3b5fbd7
Maud de La Gournerie mdelagournerie@live.fr <mdelagournerie@live.fr>
2020-10-30 18:02:53 +0100

    ajout des nouvelles pages, plus debut de la page etude de financement

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get'/mentionslegales', to: 'pages#mentionslegales'
+  get'/etudefinancement', to: 'pages#etudefinancement'
+  get'/creditimmobilier', to: 'pages#creditimmobilier'
+  get'/renegociationpret', to: 'pages#renegociationpret'
+  get'/assurancepret', to: 'pages#assurancepret'
   get'/formulaire', to: 'pages#formulaire'
   get'/actualites', to: 'pages#actualites'
   resources :users, only: [:index, :show]
