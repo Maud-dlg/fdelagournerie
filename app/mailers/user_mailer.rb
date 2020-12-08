f793226b3e3858078ba4782904886ab71bdf2546
Anne Mabire <a.b.m.mabire@gmail.com>
2020-12-08 16:55:52 +0100

    user_mailer.rb added

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,7 @@
+class UserMailer < ApplicationMailer
+  def contact(message, email)
+    @message = message
+    @email = email
+    mail(to: 'a.b.m.mabire@gmail.com', subject: 'Nouveau message depuis le formulaire de contact sur FDLG-COURTIER')
+  end
+end
