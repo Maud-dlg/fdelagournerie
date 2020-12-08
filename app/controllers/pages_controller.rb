6ff48f7a4ee7be20a4aef7f63b5f6e0e274e7252
Anne Mabire <a.b.m.mabire@gmail.com>
2020-12-08 18:16:43 +0100

    user_mailer_job.rb created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   def mentionslegales
   end
 
-  def formulaire
+  def send_formulaire
+    ContactMailerJob.perform_async(params[:message], params[:email])
+     flash[:notice] = "Votre message a bien été envoyé !"
+     redirect_back(fallback_location: root_path) # les messages flash Rails ne s'affichent qu'après rechargement de la page·
   end
 end
