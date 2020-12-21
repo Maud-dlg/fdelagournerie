0ec6f67dc36b1d056d6aafacd2307693c9fb00be
Anne Mabire <a.b.m.mabire@gmail.com>
2020-12-21 21:04:13 +0100

    gmail app password added and radio buttons in form changed to get wider selection area

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,13 +6,11 @@ class PagesController < ApplicationController
   end
 
   def send_contact
-    # ContactMailerJob.perform_async(params[:message], params[:email], params[:phone], params[:firstname], params[:lastname], params[:postcode], params[:nature], params[:avancement], params[:budget])
-    # redirect_back(fallback_location: root_path) # les messages flash Rails ne s'affichent qu'après rechargement de la page·
-    # redirect_to root_path
-    # flash[:notice] = "Votre message a bien été envoyé !"
-    # lash[:notice] = "Votre message a bien été envoyé !"
+    ContactMailerJob.perform_async(params[:message], params[:email], params[:phone], params[:firstname], params[:lastname], params[:postcode], params[:nature], params[:avancement], params[:budget])
+    redirect_to root_path
+    flash[:notice] = "Votre message a bien été envoyé !"
     # redirect_back(fallback_location: root_path) # les messages flash Rails ne s'affichent qu'après rechargement de la page·
 
-    ContactMailer.contact(params[:message]).deliver
+    # ContactMailer.contact(params[:message]).deliver
   end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,16 +1,14 @@
 class ContactMailer < ApplicationMailer
-  def contact(message)
-    # default from: "from@example.com"
-    # , email, phone, firstname, lastname, postcode, nature, avancement, budget
+  def contact(message, email, phone, firstname, lastname, postcode, nature, avancement, budget)
     @message = message
-    # @email = email
-    # @phone = phone
-    # @firstname = firstname
-    # @lastname = lastname
-    # @postcode = postcode
-    # @nature = nature
-    # @avancement = avancement
-    # @budget = budget
+    @email = email
+    @phone = phone
+    @firstname = firstname
+    @lastname = lastname
+    @postcode = postcode
+    @nature = nature
+    @avancement = avancement
+    @budget = budget
 
     mail(to: 'a.b.m.mabire@gmail.com', subject: 'Nouveau message depuis le formulaire de contact sur FDLG-COURTIER')
   end
